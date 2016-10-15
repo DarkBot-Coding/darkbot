b1ff7459251b5e9da570fb779481800501104873
DarkN-exus <itzxnexus@nexusdev.hol.es>
2016-10-15 23:44:48 +0200

    Finished all the recoding

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ bot.on('ready', () => {
   console.log("[i] Server's Names: " + bot.guilds.array().map(g => g.name).join(', '))
   console.log("[i] Legend: [i] = Information [!] = Important [C] = Command")
   console.log('--------------------------------------------------------------------------------');
-  bot.user.setStatus("online", "Recoding In Progress!");
+  bot.user.setStatus("online", "Serving " + bot.users.size + " Users On " + bot.guilds.size + " Servers And " + bot.channels.size + " Channels");
 });
 
 // create an event listener for messages
@@ -22,6 +22,7 @@ bot.on('message', message => {
   if(message.author.bot) return;
   if (!message.guild) {
   message.reply("I'm sorry, but i don't work on Private Messages :smiley:")
+  console.log('[C] ' + message.author.username + ' typed something on PM')
   }
   else {
     if (message.content.toLowerCase().startsWith(prefix + 'ping')) {
@@ -33,6 +34,61 @@ bot.on('message', message => {
                 return message.edit(`Ping took ${(endTime - startTime).toFixed(3)} ms.`);
             }).catch(console.error);
     }
+    if (message.content.toLowerCase() === 'darkbot, inviteme') {
+      message.reply('Just click here and select a server! :) https://goo.gl/am2cBq');
+      console.log('[C] ' + message.author.username + ' used darkbot, inviteme on ' + message.guild.name)
+    }
+    if (message.content.toLowerCase() === 'darkbot, joindiscord') {
+      message.reply('Join my discord server :smiley: https://discord.gg/474Hgvx');
+      console.log('[C] ' + message.author.username + ' used darkbot, joindiscord on ' + message.guild.name)
+    }
+    if (message.content.toLowerCase() === 'darkbot, quotes') {
+      message.channel.sendCode("none", "Please dont,One dumb bot is enough xD Keep yours sane :P\n-GoggleBot 13/09/2016\n\nsex is like software, its better when its free\nPosted by ProXyy on the 22/09/2016\n Source:linus torvalds\n\nImagine if androids were programmed to say Trust me, I'm an android after they finish talking lol\nOfficer: Are you human?\nAndroid: Yes, I am. Trust me, I'm an android.\nBy: Zuris on the 24/09/2016")
+      console.log('[C] ' + message.author.username + ' used darkbot, quotes on ' + message.guild.name)
+    }
+    if (message.content.toLowerCase() === 'darkbot, stats') {
+        message.channel.sendCode('none','BOT STATS\n\n' + 'Users: ' + bot.users.size + '\nServers: ' + bot.guilds.size + '\nChannels: ' + bot.channels.size);
+        console.log('[C] ' + message.author.username + ' used darkbot, stats on ' + message.guild.name)
+    }
+    if (message.content.toLowerCase() === 'darkbot, github') {
+      message.reply("Check our github: https://github.com/DarkBot-Coding/darkbot")
+      console.log('[C] ' + message.author.username + ' used darkbot, github on ' + message.guild.name)
+    }
+    if (message.content.toLowerCase() === 'darkbot, trello') {
+      message.reply("Check our trello: https://trello.com/b/9poRgtbU/darkbot")
+      console.log('[C] ' + message.author.username + ' used darkbot, trello on ' + message.guild.name)
+    }
+    if (message.content.toLowerCase().startsWith('darkmod, ban') && message.member.roles.filter(r=>r.hasPermission('BAN_MEMBERS')).size > 0) {
+    message.reply("Banned " + message.mentions.users.first())
+    console.log('[C] ' + message.author.username + ' Banned ' + message.mentions.users.first() + ' On ' + message.guild.name)
+    message.guild.ban(message.mentions.users.first(), "7");
+  }
+  if (message.content.toLowerCase().startsWith('darkowner, restart') && message.author.id === '156093486053982208') {
+    bot.user.setUsername('DarkBot MK.1')
+    .then(user => console.log(`My new username is ${user.username}`))
+    bot.user.setStatus("online", "We are on: " + bot.guilds.size + " Servers" + " With " + bot.users.size + " Users");
+    console.log("Game Reset")
+  }
+  if (message.content.toLowerCase().startsWith('darkbot, help')) {
+      message.channel.sendMessage("*DarkBot's Help*")
+      message.channel.sendCode("none","darkbot, ping: Checks if bot is online and gives you Ping in MS\ndarkbot, inviteme: Gives you and oauth link to invite the bot\ndarkbot, joindiscord: Gives you a link to his discord\ndarkbot, quotes: Shows some funny quotes\ndarkbot, stats: Shows server stats\ndarkbot, github: Gives you a link to our github\ndarkbot, trello: Gives you a link to our trello\n\n\nUse darkmod, help for mod help\nNote: Kick Member Permission Needed, Commands are case insensitive")
+      console.log('[C] ' + message.author.username + ' used darkbot, help on ' + message.guild.name)
+  }
+  if (message.content.toLowerCase().startsWith('darkmod, help')) {
+      message.channel.sendMessage("*DarkBot's Mod Help*")
+      message.channel.sendCode("none","darkmod, ban: Bans the mentioned user <darkmod, ban @User>\ndarkmod, kick: Kicks the mentioned user <darkmod, kick @User>\n\n Note: Commands are case insensitive")
+      console.log('[C] ' + message.author.username + ' used darkmod, help on ' + message.guild.name)
+  }
+  if (message.content.toLowerCase().startsWith('darkowner, restart') && message.author.id === '156093486053982208') {
+      console.log('[C] ' + message.author.username + ' shut down bot on ' + message.guild.name)
+      message.channel.sendMessage(":wave:")
+      process.exit();
+  }
+  if (message.content.toLowerCase().startsWith('darkmod, kick') && message.member.roles.filter(r=>r.hasPermission('KICK_MEMBERS')).size > 0) {
+  message.reply("Kicked " + message.mentions.users.first())
+  console.log('[C] ' + message.author.username + ' Kicked ' + message.mentions.users.first() + ' On ' + message.guild.name)
+  message.guild.member(message.mentions.users.first()).kick();
+}
   }
 
 });
